f4f9291903becaa8308a96325fde59ee8eae0dd5
igorpeclat <igor.peclat@gmail.com>
2018-11-02 14:44:15 -0300

    first

---
+++ src/main/java/com/finleap/test/weather/weatherforecastapi/WeatherForecastApiApplication.java
@@ -0,0 +1,12 @@
+package com.finleap.test.weather.weatherforecastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/finleap/test/weather/weatherforecastapi/WeatherForecastApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.finleap.test.weather.weatherforecastapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherForecastApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
